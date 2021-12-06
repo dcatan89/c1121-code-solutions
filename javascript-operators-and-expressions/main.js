267db25d38622b1515f410c70fbfae46080b6c7a
Daniel Catan <danieljcatan@gmail.com>
2021-12-06 13:01:16 -0800

    exercise complete

---
+++ javascript-operators-and-expressions/main.js
@@ -0,0 +1,41 @@
+var width = 6;
+var height = 10;
+var area = width * height;
+
+console.log('Width multiplied by Height:', area);
+console.log('typeof area:', typeof area);
+
+var bill = 2;
+var payment = 4;
+
+var change = payment - bill;
+console.log('Value of payment minus bill:', change);
+console.log('typeof change:', typeof change);
+
+var quizzes = 100;
+var midterm = 90;
+var final = 60;
+var grade = (quizzes + midterm + final) / 3;
+console.log('value of grade:', grade);
+console.log('typeof grade:', typeof grade);
+
+var firstName = 'DJ ';
+var lastName = 'Catan';
+var fullName = firstName + lastName;
+console.log('fullName:', fullName);
+console.log('typeof fullName:', typeof fullName);
+
+var pH = 4;
+var isAcidic = pH < 7;
+console.log('value of isAcidic:', isAcidic);
+console.log('type of isAcidic:', typeof isAcidic);
+
+var headCount = 100;
+var isSparta = headCount === 300;
+console.log('value of isSparta:', isSparta);
+console.log('typeof isSparta', typeof isSparta);
+
+var motto = fullName;
+motto = motto + ' is the GOAT';
+console.log('value of motto:', motto);
+console.log('typeof motto:', typeof motto);
