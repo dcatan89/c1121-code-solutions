f5717ea18468d1282b0d907e308a3b4eaeea6ab7
Daniel Catan <danieljcatan@gmail.com>
2021-12-07 12:30:36 -0800

    completed data model assignment

---
+++ javascript-data-model/order-history.js
@@ -0,0 +1,98 @@
+var orderHistory = [
+
+ oneItem : {
+
+
+    itemtype: { book: {
+
+   bookDetails: {
+        author: 'Rauschmayer, Dr. Axel',
+        title: 'JavaScript for Impatient programmers',
+        imgUrl:'',
+        isbn:''},
+
+        orderDetails: {
+        orderNumber:'114-3941689-'
+        orderDate:'August 4, 2020'
+        totalAmount:"$34.00"
+        shipTo:'JS Masher'
+        returnWindow: 'Sep 7, 2020'
+        productReviewed: false;
+                 },
+      },
+
+      {
+       bookDetails: {
+          author: 'ALexander, Christoper,
+          title: 'The TImeless Wasy of Building',
+          imgUrl: '',
+          isbn: ''
+        },
+
+        orderDetails: {
+          orderNumber: '113-9984268-'
+          orderDate: 'July 19, 2020'
+          totalAmount: "$44.53"
+          shipTo: 'JS Masher'
+          returnWindow: 'Aug 19,2020'
+          productReviewed: false
+      }
+    },
+
+    { electronic: {
+      itemDetails: {
+      itemName: 'Gamecube Controller Adapter SSB Switch Gamecube Adapter for Wii U,.',
+      imgURL:'',
+      },
+
+    orderdetails: {
+      orderNumber:'114-2875557-'
+        orderDate:
+      totalAmount:"$17.22"
+        shipTo: 'JS MASHER'
+      returnWindow: 'Aug 5, 2020'
+        productReviewed: false;}
+    }
+  }
+}
+},
+
+
+ twoItems: { fullOrderDetails: {
+  orderNumber:'113-2883177-2648248',
+  orderDate: 'July 3, 2020',
+  totalAmount: '$138.93',
+  shipTo: 'JS Masher',
+  itemTypes: {
+  book: [ {
+    title: 'The Art of Sql'
+    author:'Faroult, Stephanie'
+    isbn:''
+  },
+
+  {
+    price: '$33.99'
+    returnWindow: 'Aug 4, 2020',
+    productReviewed: false}
+  ],
+
+  electronics: [
+    {
+      itemName:'Game Cube Controller- Super Smash Edition(nintendoswitch',
+      imgURL:'',
+    },
+
+    {
+      returnWindow: 'Aug 4, 2020',
+      productReviewed: false
+      price: '$94.95'
+
+    };
+    ]
+  };
+  }
+  }
+
+
+
+]
