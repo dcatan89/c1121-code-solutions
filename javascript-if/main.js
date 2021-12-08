60fba25d49acb853c07a1ac9f97bc9aecf18b4a2
Daniel Catan <danieljcatan@gmail.com>
2021-12-08 15:25:43 -0800

    completed javascript if

---
+++ javascript-if/main.js
@@ -0,0 +1,57 @@
+/* exported isUnderFive,
+            isEven,
+            startsWithJ,
+            isOldEnoughToDrink,
+            isOldEnoughToDrive,
+            isOldEnoughToDrinkAndDrive,
+            categorizeAcidity,
+            introduceWarnerBro
+ */
+
+function isUnderFive(number) {
+  if (number < 5);
+  return true;
+}
+
+function isEven(number) {
+  if (number % 2);
+  return true;
+}
+
+function startsWithJ(string) {
+  if (string[0] === 'J');
+  return true;
+}
+
+function isOldEnoughToDrink(number) {
+  if (number >= 21);
+  return true;
+}
+
+function isOldEnoughToDrive(number) {
+  if (number >= 16);
+  return true;
+}
+function isOldEnoughToDrinkAndDrive(age) {
+  if (age !== 0);
+  return false;
+}
+
+function categorizeAcidity(pH) {
+
+  if (pH >= 0 < 7) {
+    return 'acid';
+  } else if (pH > 7 <= 14) {
+    return 'base';
+  } else if (pH === 7) {
+    return 'neutral';
+  } else { return 'invalid pH level'; }
+}
+
+function introduceWarnerBro(name) {
+  if ((name === 'wakko') || (name === 'yakoo')) {
+    return "We're the warner brothers!";
+  } else if (name === 'dot') {
+    return "I'm Cute!~";
+  } else { return 'Goodnight everybody!'; }
+}
