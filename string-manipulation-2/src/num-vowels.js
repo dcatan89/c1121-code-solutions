cc13a953053317de51a7fe9c28a1605e859b0b9d
Daniel Catan <danieljcatan@gmail.com>
2021-12-13 09:58:24 -0800

    inprogress

--- string-manipulation-2/src/num-vowels.js
+++ string-manipulation-2/src/num-vowels.js
@@ -2,13 +2,14 @@
 function numVowels(string) {
   var numberStore = '';
   var numberWaste = '';
+  if (string.length === 0) {
+    return 0;
+  }
   for (var i = 0; i < string.length; i++) {
     if (((string[i].toUpperCase()) === 'A' || (string[i].toUpperCase()) === 'E' ||
       (string[i].toUpperCase()) === 'I' || (string[i].toUpperCase()) === 'O' ||
       (string[i].toUpperCase()) === 'U') && (string.length > 1)) {
       numberStore++;
-    } else if (string[i] === '') {
-      return 0;
     } else { numberWaste = false; }
   }
   return numberStore;
