e04db41b105d830ad05d4b28ca428c32b09b3742
Daniel Catan <danieljcatan@gmail.com>
2021-12-15 14:37:01 -0800

    fixed issues

--- javascript-forms/form-events.js
+++ javascript-forms/form-events.js
@@ -12,9 +12,9 @@ function handleInput(event) {
   console.log('eventName:', event.target.name, 'eventValue:', event.target.value);
 }
 
-var focusEvent = document.querySelector('input');
-var blurEvent = document.querySelector('input');
-var inputEvent = document.querySelector('input');
+var focusEvent = document.querySelector('form');
+var blurEvent = document.querySelector('form');
+var inputEvent = document.querySelector('form');
 
 focusEvent.addEventListener('focus', handleFocus);
 blurEvent.addEventListener('blur', handleBlur);
