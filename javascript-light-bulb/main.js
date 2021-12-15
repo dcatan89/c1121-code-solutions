408f7773473a61e96118a09c10b317cc0d9b0cdb
Daniel Catan <danieljcatan@gmail.com>
2021-12-15 13:03:33 -0800

    made into endless clicker

--- javascript-light-bulb/main.js
+++ javascript-light-bulb/main.js
@@ -1,9 +1,15 @@
 var lightBulbClicker = document.querySelector('.light-bulb-on');
 var background = document.querySelector('.light-background');
-
+var i = 0;
 function onOff(event) {
-  lightBulbClicker.className = 'light-bulb-off';
-  background.className = 'black-background';
+  i++;
+  if (i % 2 === 0) {
+    lightBulbClicker.className = 'light-bulb-on';
+    background.className = 'light-background';
+  } else {
+    lightBulbClicker.className = 'light-bulb-off';
+    background.className = 'black-background';
+  }
 
 }
 
