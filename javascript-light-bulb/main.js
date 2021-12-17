dbd543fddd43fc6a403ac3c1c35c0d247b37c5ff
Daniel Catan <danieljcatan@gmail.com>
2021-12-17 12:00:25 -0800

    fixed variable name

--- javascript-light-bulb/main.js
+++ javascript-light-bulb/main.js
@@ -1,17 +1,17 @@
-var lightBulbClicker = document.querySelector('.light-bulb-on');
+var lightOn = document.querySelector('.light-bulb-on');
 var background = document.querySelector('.light-background');
 var i = true;
 function onOff(event) {
   if (i === true) {
-    lightBulbClicker.className = 'light-bulb-on';
+    lightOn.className = 'light-bulb-on';
     background.className = 'light-background';
     i = false;
   } else {
-    lightBulbClicker.className = 'light-bulb-off';
+    lightOn.className = 'light-bulb-off';
     background.className = 'black-background';
     i = true;
   }
 
 }
 
-lightBulbClicker.addEventListener('click', onOff);
+lightOn.addEventListener('click', onOff);
