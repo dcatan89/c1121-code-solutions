d72f2348864afb99ec0eb1621f8ecfe2405bd4b9
Daniel Catan <danieljcatan@gmail.com>
2021-12-17 11:58:35 -0800

    in progress

--- object-manipulation-2/src/pick.js
+++ object-manipulation-2/src/pick.js
@@ -1,18 +1,20 @@
 /* exported pick */
 // make storage for the output
 // check if the array values are properties in the object
+// Check ove3r each value one by one to see if key is a property
+// if true store the source.key[key[i]]
 //  if true store the object property and values.
 //  if false ,return and do not include it in the array.
 // Return the storage as output,
 
 function pick(source, key) {
-  var objectOutput ={};
-for (var prop in source) {
+  var objectOutput = {};
   for (var i = 0; i < key.length; i++) {
-      if (source[key[i]] === key[i]) {
-        objectOutput =
-          key[i] : source[property];
-      } else { false; }
+    if (!source[key[i]]) {
+      key[i] = false;
+    } else {
+      objectOutput += source.key[i];
+      source.key[i] = i + 1;
     }
   }
 
