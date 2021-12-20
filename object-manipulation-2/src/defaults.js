5e92b1b7e6305277277c37524eb17720fb532dbd
Daniel Catan <danieljcatan@gmail.com>
2021-12-19 17:16:35 -0800

    in progress

--- object-manipulation-2/src/defaults.js
+++ object-manipulation-2/src/defaults.js
@@ -1,1 +1,22 @@
 /* exported defaults */
+// -Check to see one by to see if there are any matching properties between target object and source object
+//  -If there are any, replace the properties that match from source to be the same as the target object
+//  -replace and combine the two objects by keeping the values of target and then adding new ones from source
+// console log the new object literal of target
+function defaults(target, source) {
+  var newObj = {};
+
+  for (var prop in source) {
+    if (source[prop]) {
+      newObj[prop] = source[prop];
+    }
+  }
+
+  for (prop in target) {
+    if (target[prop]) {
+      newObj[prop] = target[prop];
+    }
+  }
+
+  console.log(newObj);
+}
--- object-manipulation-2/src/omit.js
+++ object-manipulation-2/src/omit.js
@@ -4,12 +4,8 @@ function omit(source, key) {
   var objectOutput = {};
   for (var prop in source) {
     for (var i = 0; i <= key.length; i++) {
-      if (prop !== key[i]) {
-        objectOutput[prop] = source[prop];
-      } else { delete objectOutput[key[i]]; }
+      if (prop === key[i]) {
+        delete objectOutput[key[i]];
+      }
     }
-  }
-
-  return objectOutput;
-
 }
