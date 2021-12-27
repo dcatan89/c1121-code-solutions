fd6955f636a964fa5d955b0f5d02f53a5961585d
Daniel Catan <danieljcatan@gmail.com>
2021-12-26 16:59:27 -0800

    changed var names and transcribed psuedo code

--- array-manipulation-2/src/drop.js
+++ array-manipulation-2/src/drop.js
@@ -1,10 +1,15 @@
 /* exported drop */
+// make a new array storage for the output
+// check each value of the array and stop once the number of values checked reaches the count
+// check to see if the count is greater than the values within the array
+// if true return the array values as ouput
+//  otherwise put the values into the new array and stop at the count
 function drop(array, count) {
-  var micDrop = [];
+  var numberDrop = [];
   for (var i = 0; i < array.length - count; i++) {
     if (count > array.length) {
       return array;
-    } else { micDrop.push(array[i + count]); }
+    } else { numberDrop.push(array[i + count]); }
   }
-  return micDrop;
+  return numberDrop;
 }
--- array-manipulation-2/src/take.js
+++ array-manipulation-2/src/take.js
@@ -1,4 +1,9 @@
 /* exported take */
+// Create storage for a new array
+// Check to each value one by one to see if the the amount count is larger than the array size
+//  if true return the array value
+//  Otherwise count how many values of the array is compared to the given count then store it
+// return the storage as output
 function take(array, count) {
   var newArr = [];
   for (var i = 0; i < count; i++) {
