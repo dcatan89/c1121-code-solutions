9acf04b13bbf36c733ffab9748429f184b93aee1
Daniel Catan <danieljcatan@gmail.com>
2021-12-27 12:26:50 -0800

    javascript-this completed exercise

---
+++ javascript-this/student.js
@@ -0,0 +1,13 @@
+/* exported student */
+var student = {
+  firstName: 'DeeJay',
+  lastName: 'Catan',
+  subject: 'JavaScript',
+  getFullName: function () {
+    var fullName = this.firstName + ' ' + this.lastName;
+    return fullName;
+  },
+  getIntroduction: function () {
+    return 'Hello, my name is ' + this.getFullName() + ' and I am studying ' + this.subject + '.';
+  }
+};
