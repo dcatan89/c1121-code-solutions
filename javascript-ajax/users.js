24b854e25975478e4a164bd98781aee48d333a46
dcatan89 <danieljcatan@gmail.com>
2021-12-31 23:04:00 +0000

    ajax completed

--- javascript-ajax/users.js
+++ javascript-ajax/users.js
@@ -1,17 +1,14 @@
 var $userList = document.querySelector('#user-list');
-
-function userData(name) {
-  var xhr = new XMLHttpRequest();
-  xhr.open('GET', 'https://jsonplaceholder.typicode.com/users');
-  xhr.responseType = 'json';
-  xhr.addEventListener('load', function () {
-    console.log(xhr.status);
-    console.log(xhr.response);
-    for (var i = 0; i < xhr.response; i++) {
-      var $li = document.createElement('li');
-      $li[i].textContent = name;
-      $userList.appendchild($li[i]);
-    }
-  });
-  xhr.send();
-}
+var xhr = new XMLHttpRequest();
+xhr.open('GET', 'https://jsonplaceholder.typicode.com/users');
+xhr.responseType = 'json';
+xhr.addEventListener('load', function () {
+  console.log(xhr.status);
+  console.log(xhr.response);
+  for (var i = 0; i < xhr.response.length; i++) {
+    var $li = document.createElement('li');
+    $li.textContent = xhr.response[i].name;
+    $userList.appendChild($li);
+  }
+});
+xhr.send();
